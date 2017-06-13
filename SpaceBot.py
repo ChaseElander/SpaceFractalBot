e14fa1cbd12f45cc23fda9eb717eb03ca5361e91
Chase Elander <chaseelander@gmail.com>
2017-06-12 22:37:55 -0500

    added getem.py from user madprops on Stack Overflow. Will be analyzing how their code works and attempting to reverse engineer and then automate.

--- SpaceBot.py
+++ SpaceBot.py
@@ -1,12 +1,13 @@
 import csv
 import requests
+import urllib2
+import shutil
+import sys
+import time
 from BeautifulSoup import BeautifulSoup
 
 url = 'https://apod.nasa.gov/apod/astropix.html'
 response = requests.get(url)
 html = response.content
 
-#print html
-soup = BeautifulSoup(html)
-table = soup.find('img')
-
+print html
